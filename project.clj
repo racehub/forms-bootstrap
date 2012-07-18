32f2b9b525e05fadf6692127ba72b8b1776bb51b
David Petrovics <dpetrovics@gmail.com>
2012-07-18 12:43:04 -0400

    added css, html template, and core

--- project.clj
+++ project.clj
@@ -1,3 +1,9 @@
-(defproject forms-bootstrap "1.0.0-SNAPSHOT"
+(defproject forms-bootstrap "0.1.0-SNAPSHOT"
   :description "Utility for creating web forms using Twitter's Bootstrap CSS"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [noir "1.3.0-beta3"]
+                 [enlive "1.0.1"]
+                 [org.clojars.dpetrovics/sandbar "0.4.0-SNAPSHOT"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
+  :source-path "src")
