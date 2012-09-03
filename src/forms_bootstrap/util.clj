666c53323cb03e24b466443eb203ffb5ffb7fa26
David Petrovics <dpetrovics@gmail.com>
2012-09-03 09:15:27 -0400

    Huge refactor of core. You can make a lite version (for inline forms) and a regular version (For horizontal forms) for each form element. The regular version contains the lite version, but is wrapped in a controls div and a control group div. Upgraded to bootstrap 2.1. Updated tests.

---
+++ src/forms_bootstrap/util.clj
@@ -0,0 +1,32 @@
+(ns forms-bootstrap.util
+  (:require [clojure.string :as string]))
+
+(defn remove-spaces
+  "Remove spaces from a string"
+  [input]
+  (string/replace input " " ""))
+
+(defn string-contains?
+  "Takes in a string and any number of search words. Returns true if all of the search words can be found in the input string. "
+  [sentence & search-words]
+  (let [sent-freq (frequencies (re-seq #"[^\s]+" sentence))
+        matches (filter true?
+                        (map #(contains? sent-freq %1) search-words))]
+    (= (count matches) (count search-words))))
+
+(defn first-word
+  "Takes an input string and returns the first word in it"
+  [x]
+  (clojure.core/get (string/split x #"\s") 0))
+
+(defn collectify
+  "Takes in one argument, and returns it if its already a vector,
+  otherwise it makes it into one."
+  [x]
+  (if (sequential? x) x [x]))
+
+(defn insert
+  "Takes in a vector, a position, and a value. Returns a new vector
+  with the value inserted at the given position."
+  [v pos val]
+  (apply conj (subvec v 0 pos) val (subvec v pos)))
--- test/forms_bootstrap/test/core.clj
+++ test/forms_bootstrap/test/core.clj
@@ -1,7 +1,7 @@
 (ns forms-bootstrap.test.core
   (:use forms-bootstrap.core
         clojure.test
-        forms-bootstrap.core
+        forms-bootstrap.util
         noir.core
         net.cgrand.enlive-html)
   (:require [noir.response :as response]
@@ -32,15 +32,29 @@
                    :action "someaction"
                    :submit-label "Send it!"
                    :cancel-link "/"
-                   :fields [{:type "text" :name "nickname" :label "Nick Name" :size "input-large"}
-                            {:type "password" :name "password" :label "Password"}
-                            {:type "text" :name "city" :label "City" :placeholder "Placeholder!"}
-                            {:type "text-area" :name "description" :label "Favorite Quote"}
-                            {:type "select" :name "colors" :label "Favorite Color"
+                   :fields [{:type "text"
+                             :name "nickname"
+                             :label "Nick Name"
+                             :size "input-large"}
+                            {:type "password"
+                             :name "password"
+                             :label "Password"}
+                            {:type "text"
+                             :name "city"
+                             :label "City"
+                             :placeholder "Placeholder!"}
+                            {:type "text-area"
+                             :name "description"
+                             :label "Favorite Quote"}
+                            {:type "select"
+                             :name "colors"
+                             :label "Favorite Color"
                              :inputs [["blue" "Blue"]
                                       ["red" "Red"]
                                       ["yellow" "Yellow"]]}
-                            {:type "radio" :name "cars" :label "Favorite Car"
+                            {:type "radio"
+                             :name "cars"
+                             :label "Favorite Car"
                              :inputs [["honda" "Honda"]
                                       ["toyota" "Toyota"]
                                       ["chevy" "Chevy"]]}
@@ -48,10 +62,11 @@
                              :inputs [["german" "German"]
                                       ["french" "French"]
                                       ["english" "English"]]}
-                            {:type "file-input" :name "afile" :label "Choose a pic"}])
+                            {:type "file-input"
+                             :name "afile"
+                             :label "Choose a pic"}])
                   "Example One"
-                  "How to use make-form"]]
-                }))
+                  "How to use make-form"]]}))
 
 (defn email-valid?
   [{:keys [email] :as m}]
@@ -68,19 +83,50 @@
              :submit-label "Sign Up!"
              :fields [{:name "first-name"
                        :label "First Name"
+                       :help-inline "This is 'help-inline' for the first name field."
                        :type "text"}
                       {:name "last-name"
+                       :help-block "This is 'help-block' for the last
+                       name field. You can fit more stuff like this."
                        :label "Last Name"
                        :type "text"}
                       {:name "gender"
-                       :label "Gender"
-                       :type "radio"
+                       :label "Gender" 
+                       :type "radio inline"
                        :inputs [["male" "Male"]
                                 ["female" "Female"]]}
+                      {:name "options"
+                       :label "Options"
+                       :type "select"
+                       :inputs [["option1" "Option 1"]
+                                ["option2" "Option 2"]
+                                ["option3" "Option 3"]]}
                       {:name "email"
                        :label "Email Address"
                        :type "text"
                        :placeholder "Try using 'blah'"}
+                      {:type "inline-fields"
+                       :name "birthday"
+                       :label "Birthday"
+                       :columns [{:name "birthday-day"
+                                  :type "select"
+                                  :size "input-small" 
+                                  :inputs (let [days (reduce #(conj %1 [(str %2) (str %2)])
+                                                             [] (range 1 32))]
+                                            (insert days 0 ["" "Day"]))}
+                                 {:name "birthday-month"
+                                  :type "select"
+                                  :size "input-small" 
+                                  :inputs (let [days (reduce #(conj %1 [(str %2) (str %2)])
+                                                             [] (range 1 13))]
+                                            (insert days 0 ["" "Month"]))}
+                                 {:name "birthday-year"
+                                  :type "select"
+                                  :size "input-small"
+                                  :inputs (let [year (reduce #(conj %1 [(str %2) (str %2)])
+                                                             [] (reverse
+                                                                 (range 1900 2013)))]
+                                            (insert year 0 ["" "Year"]))}]}
                       {:name "colors[]"
                        :label "Favorite Colors"
                        :type "checkbox"
