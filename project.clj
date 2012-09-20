08c3a80b63d377b2cdc2cb7db5e3699db37383e9
David Petrovics <dpetrovics@gmail.com>
2012-09-20 00:22:27 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject forms-bootstrap "0.3.1"
+(defproject forms-bootstrap "0.4.0-SNAPSHOT"
   :description "Utility for creating web forms using Twitter's Bootstrap CSS"
   :url "https://github.com/dpetrovics/forms-bootstrap"
   :dependencies [[org.clojure/clojure "1.4.0"]
