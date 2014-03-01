83d49cef13c53b3b404a80578ae7d09a7ec8d125
Sam Ritchie <sritchie09@gmail.com>
2014-03-01 06:08:46 -0700

    saving.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paddleguru/forms-bootstrap "0.7.1"
+(defproject paddleguru/forms-bootstrap "0.7.2"
   :description "Utility for creating web forms using Twitter's Bootstrap CSS"
   :url "https://github.com/paddleguru/forms-bootstrap"
   :license {:name "Apache License, Version 2.0"
