6b43e8dd02cf8b4f18b6054df6de364d3eeed12b
Sam Ritchie <sritchie09@gmail.com>
2014-07-04 17:38:25 -0600

    add gitflow

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paddleguru/forms-bootstrap "0.9.0"
+(defproject paddleguru/forms-bootstrap "0.9.0-SNAPSHOT"
   :description "Utility for creating web forms using Twitter's Bootstrap CSS"
   :url "https://github.com/paddleguru/forms-bootstrap"
   :license {:name "Apache License, Version 2.0"
@@ -7,7 +7,8 @@
                  [lib-noir "0.8.4" :exclusions [ring/ring]]
                  [enlive "1.1.5"]]
   :main forms-bootstrap.server
-  :profiles {:dev {:dependencies [[compojure "1.1.8"]
+  :profiles {:dev {:plugins [[paddleguru/lein-gitflow "0.1.2"]]
+                   :dependencies [[compojure "1.1.8"]
                                   [http-kit "2.1.18"]
                                   [javax.servlet/servlet-api "2.5"]
                                   [ring/ring-core "1.3.0"]
