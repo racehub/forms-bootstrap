27e533be0bb2320e2a67ce394d95735739469018
Sam Ritchie <sritchie09@gmail.com>
2014-07-04 17:31:48 -0600

    saving.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject paddleguru/forms-bootstrap "0.8.1"
+(defproject paddleguru/forms-bootstrap "0.9.0"
   :description "Utility for creating web forms using Twitter's Bootstrap CSS"
   :url "https://github.com/paddleguru/forms-bootstrap"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.7.5"]
-                 [enlive "1.1.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [lib-noir "0.8.4" :exclusions [ring/ring]]
+                 [enlive "1.1.5"]]
   :main forms-bootstrap.server
   :profiles {:dev {:dependencies [[compojure "1.1.5"]
                                   [http-kit "2.1.13"]
